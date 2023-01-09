96a2070992f96c6b725a9c843436e921139f31b2
Alex Roman <alexandrupero@users.noreply.github.com>
2023-01-09 17:25:02 +0000

    fix tests

--- test/cv-console.test.ts
+++ test/cv-console.test.ts
@@ -9,6 +9,12 @@ describe('CvConsole', () => {
     element = await fixture(html`<cv-console></cv-console>`);
   });
 
+  it('is defined', () => {
+    // Without using an instance of CvConsole here, the compile process would throw out the import statement.
+    const cvConsoleElement = document.createElement('cv-console');
+    expect(cvConsoleElement).to.be.instanceof(CvConsole);
+  });
+
   it('renders a h1', () => {
     const h1 = element.shadowRoot!.querySelector('h1')!;
     expect(h1).to.exist;
@@ -17,5 +23,9 @@ describe('CvConsole', () => {
 
   it('passes the a11y audit', async () => {
     await expect(element).shadowDom.to.be.accessible();
+  });
+
+  it('has styling applied', async () => {
+    expect(getComputedStyle(element).maxWidth).to.equal('960px');
   });
 });
