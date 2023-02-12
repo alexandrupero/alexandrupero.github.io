4a8d151574cc3c5d9c5be6cba78db63acedee702
alexandrupero <alexandrupero@users.noreply.github.com>
2023-02-12 17:59:52 +0000

    refactor: switch to nuxt 3

---
+++ layouts/default.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <header class="shadow-sm bg-white">
+      <nav class="container mx-auto p-4 flex justify-between">
+        <NuxtLink class="font-bold" to="/">alexandrupero.github.io</NuxtLink>
+        <ul class="flex gap-4">
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+
+    <div class="container mx-auto p-4">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.router-link-exact-active {
+  color: #12b488;
+}
+</style>
