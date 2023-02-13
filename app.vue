d01ce025ed563c6015dabc5eefa9e298074f21cf
alexandrupero <alexandrupero@users.noreply.github.com>
2023-02-13 14:38:35 +0000

    style: add daisyui and create page structure

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <NuxtLayout>
-      <NuxtPage />
+      <CvTerminal />
     </NuxtLayout>
   </div>
 </template>
---
+++ components/CvTerminal.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    class="mockup-code bg-base-300 text-base-content text-sm items-stretch overflow-x-clip h-full w-full"
+  >
+    <pre><code>   ________________________
+    / Hi there, it's nice to \
+    \ virtually meet you!    /
+     ------------------------
+      \
+       \
+       |\_/|
+       |o o|__
+       --*--__\
+       C_C_(___)
+       <pre><code>  Available commands:
+      bla - blabla
+</code></pre>
+</code></pre>
+    <pre
+      data-prefix="$"
+    ><input type="text" placeholder="Type here" class="terminal-input input-sm input-ghost bg-base-300 w-11/12" autofocus /></pre>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.terminal-input:focus {
+  outline: none;
+  border: none;
+}
+</style>
