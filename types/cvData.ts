ed90aab12785b5c843c9147ae0db9f7725360353
alexandrupero <alexandrupero@users.noreply.github.com>
2023-02-18 12:32:41 +0000

    feat!: add cv data and nuxt css icons

---
+++ types/cvData.ts
@@ -0,0 +1,24 @@
+export interface CvContactItem {
+  github: string
+  linkedin: string
+  instagram: string
+}
+
+export interface CvExperienceItem {
+  company: string
+  role: string
+  location: string
+  startDate: Date
+  endDate: Date | null
+  responsibilities: string[]
+}
+
+export interface CvData {
+  name: string
+  languages: string[]
+  frameworks: string[]
+  tools: string[]
+  skills: string[]
+  experience: CvExperienceItem[]
+  contact: CvContactItem
+}
